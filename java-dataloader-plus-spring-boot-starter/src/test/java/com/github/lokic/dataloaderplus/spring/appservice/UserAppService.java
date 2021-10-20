641d4390705894c4e222031d1b6122aa1d6863e4
loki <kiraconan2046@gmail.com>
2021-10-20 19:17:01 +0800

    feat: 优化部分代码

--- java-dataloader-plus-spring-boot-starter/src/test/java/com/github/lokic/dataloaderplus/spring/appservice/UserAppService.java
+++ java-dataloader-plus-spring-boot-starter/src/test/java/com/github/lokic/dataloaderplus/spring/appservice/UserAppService.java
@@ -2,12 +2,16 @@ package com.github.lokic.dataloaderplus.spring.appservice;
 
 import com.github.lokic.dataloaderplus.spring.annotation.DataLoadable;
 import com.github.lokic.dataloaderplus.spring.client.UserClient;
+import com.github.lokic.dataloaderplus.spring.entity.UserInfo;
 import com.github.lokic.dataloaderplus.spring.service.UserService;
+import com.github.lokic.javaplus.CompletableFutures;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.StringJoiner;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import static com.github.lokic.javaplus.CompletableFutures.Fors.For;
 import static com.github.lokic.javaplus.CompletableFutures.Fors.Yield;
@@ -37,6 +41,21 @@ public class UserAppService {
                 .thenCompose(For((id, name) -> userClient.getAddressById(id)))
                 .thenApply(Yield((id, name, address) ->
                         new StringJoiner(",").add(id).add(name).add(address).toString()));
+    }
+
+    @DataLoadable
+    public CompletableFuture<UserInfo> getUserInfo(String uid) {
+        return userClient.getNameById(uid)
+                .thenCompose(For((name) -> userClient.getAddressById(uid)))
+                .thenApply(Yield((name, address) -> new UserInfo(uid, name, address)));
+    }
+
+    @DataLoadable
+    public CompletableFuture<List<UserInfo>> getUserInfoList(List<String> uidList) {
+        List<CompletableFuture<UserInfo>> li = uidList.stream()
+                .map(this::getUserInfo)
+                .collect(Collectors.toList());
+        return CompletableFutures.sequence(li);
     }
 
     @DataLoadable
